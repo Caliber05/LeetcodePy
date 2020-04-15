1a5b5daf4532428017b0297f26633f6d2f71ccf0
Sanmesh Potdar <60248202+Caliber05@users.noreply.github.com>
2020-04-15 19:43:49 +0530

    Update singlenumber.py

--- singlenumber.py
+++ singlenumber.py
@@ -11,4 +11,8 @@ class Solution(object):
             else:
                 no_duplicate_list.remove(i)
         return no_duplicate_list.pop()
+    
+    //  Time complexity : O(n^2)O(n). We iterate through \text{nums}nums, taking O(n)O(n) time. We search the whole list to find whether there is duplicate number, 
+        taking O(n)O(n) time. Because search is in the for loop, so we have to multiply both time complexities which is O(n^2)O(n).
+        Space complexity : O(n)O(n). We need a list of size nn to contain elements in \text{nums}nums.//
         
